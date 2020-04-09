6c3792a02e0f4eb25dc0e2dd4760bac2c7e017be
Anastasia326 <60912054+Anastasia326@users.noreply.github.com>
2020-04-09 22:46:07 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -22,8 +22,8 @@ def draw_button(colour: tuple, coordinates: tuple, name: str, font: int = 40):
                       coordinates[1],
                       coordinates[2],
                       coordinates[3]))
-    fond = pygame.font.Font(None, 40)
-    text = fond.render(name, True, [0, 0, 0])
+    fond = pygame.font.Font(None, 35)
+    text = fond.render(name, True, [218, 165, 32])
     window.blit(text, [coordinates[0] + coordinates[2] // 100 + 1, coordinates[
         1] + coordinates[3] // 100 + 1])
 
