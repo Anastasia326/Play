7d238390e0804da2ee97044e7a58b08f7c6d40f4
Anastasia <kemova.a.y@list.ru>
2020-05-15 21:08:36 +0300

    text under the field

--- Working_with_textures/choose_units_to_kill.py
+++ Working_with_textures/choose_units_to_kill.py
@@ -18,15 +18,14 @@ def choose_units_to_kill(window, fullscreen, units, length=800, height=600,
                                     [units[0].base.name, units[1].base.name,
                                      units[2].base.name, units[3].base.name],
                                     (0, 0, size, size_of_cell * 4),
-                                    255, 215, 0, 85, 107, 47, int(15 * k))
+                                    255, 215, 0, 85, 107, 47, int(10 * k))
         names = []
         for i in units[4:]:
             names += [i.base.name]
         buttons += draw_some_buttons(window, len(names), names,
-                                     (
-                                         length - size, 0, size,
+                                     (length - size, 0, size,
                                          size_of_cell * 4),
-                                     255, 215, 0, 85, 107, 47, int(15 * k))
+                                     255, 215, 0, 85, 107, 47, int(10 * k))
     else:
         names = []
         for i in units:
@@ -34,7 +33,7 @@ def choose_units_to_kill(window, fullscreen, units, length=800, height=600,
         buttons = draw_some_buttons(window, len(names),
                                     names,
                                     (0, 0, 100, 200),
-                                    255, 215, 0, 85, 107, 47, int(15 * k))
+                                    255, 215, 0, 85, 107, 47, int(10 * k))
 
     buttons += draw_some_buttons(window, 1, ["End"],
                                  (length - size, size_of_cell * 4 + 10, size,
--- Working_with_textures/write_what_happened.py
+++ Working_with_textures/write_what_happened.py
@@ -1,10 +1,12 @@
 import pygame
 
-
 def write_what_happened(text, window, length = 800, height = 600, k = 1):
-    font = pygame.font.SysFont('dejavuserif', int(40 * k))
-    text_ = font.render(text, True, (255, 215, 0))
-    size_of_cell = (height * 5 // 6) // 10
-    size = (length - size_of_cell * 12) // 2
-    window.blit(text_, (size, (height // 6) * 5))
+    phrases = text.split("/")
+    for i in range(len(phrases)):
+        font = pygame.font.SysFont('dejavuserif', int(20 * k))
+        text_ = font.render(phrases[i], True, (255, 215, 0))
+        size_of_cell = (height * 5 // 6) // 10
+        size = (length - size_of_cell * 12) // 2
+        window.blit(text_, (size, (height // 6) * 5 + int(20 * k) * i))
     pygame.display.update()
+
