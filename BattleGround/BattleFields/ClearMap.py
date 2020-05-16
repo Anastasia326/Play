9139333521ea265efcefc3d6a742b85726e6d224
FArthur-cmd <613623@mail.ru>
2020-05-16 21:41:23 +0300

    Creating final executable version

--- BattleGround/BattleFields/ClearMap.py
+++
@@ -1,6 +0,0 @@
-from BattleGround.CreateBattleField import BattleField
-
-
-class ClearMap(BattleField):
-    def make_objects(self) -> str:
-        return "no objects on map"
--- BattleGround/CreateBattleField.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BattleField(ABC):
-    field = [[None for i in range(12)] for j in range(10)]
-
-    @abstractmethod
-    def make_objects(self) -> str:
-        pass
